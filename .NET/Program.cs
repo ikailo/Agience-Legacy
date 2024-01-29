f4535191b6fc2f4422debd5d3544ac743f9aad98
john-s4d <john@ikailo.com>
2024-01-29 00:31:00 -0500

    in progress

--- .NET/Program.cs
+++ .NET/Program.cs
@@ -5,30 +5,25 @@ namespace Agience.Agents_Console
 {
     internal class Program
     {
-
-        private static Agent? _agent;
+        private static readonly AppConfig _config = new();
 
         internal static async Task Main(string[] args)
         {
-            var _config = new AppConfig();
-
             var instance = new Instance(_config);
 
             //instance.LogMessage += LogMessage_callback;
 
-            instance.AgentConnected += Instance_AgentConnected;            
+            instance.AgentConnected += Agent_Connected;
+            instance.AgencyConnected += Agency_Connected;
 
             instance.Catalog.Add(agent => new Debug(agent));
             instance.Catalog.Add(() => new InteractWithUser());
             instance.Catalog.Add(() => new GetInputFromUser());
-            //instance.Catalog.Add(() => new ShowMessageToUser(ShowMessageToUser_callback));
+            instance.Catalog.Add(() => new ShowMessageToUser(ShowMessageToUser_callback));
 
             await instance.Connect();
 
             /*
-            // Pick an agent to work with. Here we'll just get one that's defined in the config.
-            _agent = instance.Agents.Where(agent => agent.Agency?.Id == _config.AgentId).FirstOrDefault();
-
             if (_agent != null)
             {
                 _agent.Publish("<context setup>");
@@ -39,36 +34,45 @@ namespace Agience.Agents_Console
             do { await Task.Delay(10); } while (instance.IsConnected);
         }
 
-        private static async Task Instance_AgentConnected(Agent agent)
+        private static async Task Agent_Connected(Agent agent)
         {
-            Console.WriteLine($"{agent.Id} {agent.Name} Connected");
+            if (agent.Id == _config.AgentId)
+            {
+                Console.WriteLine($"{agent.Id} {agent.Name} Connected");
+                agent.Invoke<GetInputFromUser>();
+            }
         }
 
-        /*
-private static void LogMessage_callback(object? sender, string message)
-{
-   Console.WriteLine($"{_agent?.Name ?? "Interaction.Local"} | {message}");
-}
+        private static async Task Agency_Connected(Agency ageny)
+        {
+            Console.WriteLine($"{ageny.Id} {ageny.Name} Connected");
+        }
+        
+        private static void LogMessage_callback(object? sender, string message)
+        {
+            throw new NotImplementedException();
+            //Console.WriteLine($"{_agent?.Name ?? "Interaction.Local"} | {message}");
+        }
 
-private async static Task InteractWithUser_callback(Data? userInput)
-{
-   if (_agent?.Instance == null) { return; }
+        private async static Task InteractWithUser_callback(Agent agent, Data? userInput)
+        {
+            if (agent?.Instance == null) { return; }
 
-   if (userInput?.Raw?.Equals("quit", StringComparison.OrdinalIgnoreCase) ?? false)
-   {
-       await _agent.Instance.Disconnect();
+            if (userInput?.Raw?.Equals("quit", StringComparison.OrdinalIgnoreCase) ?? false)
+            {
+                await agent.Instance.Disconnect();
 
-       Console.WriteLine($"{_agent.Instance.Name} Shut Down");
-   }
-   else
-   {
-       _agent.Prompt(userInput, "Interact with the user.", InteractWithUser_callback);
-   }
-}
+                Console.WriteLine($"{agent.Instance.Name} Shut Down");
+            }
+            else
+            {
+                agent.Prompt(userInput, "Interact with the user.", InteractWithUser_callback);
+            }
+        }
 
-private static void ShowMessageToUser_callback(string? message)
-{
-   Console.Write($"{(string.IsNullOrEmpty(message) ? string.Empty : $"{message}")}");
-}*/
+        private static void ShowMessageToUser_callback(string? message)
+        {
+            Console.Write($"{(string.IsNullOrEmpty(message) ? string.Empty : $"{message}")}");
+        }
     }
 }
