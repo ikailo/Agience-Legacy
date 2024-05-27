727a591e720017d40f00571d9ad3b6eef3d961dc
john-s4d <john@ikailo.com>
2024-05-27 12:49:06 -0400

    prepare for consolidation

--- Hosts/Console/Plugins/IConsoleService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Agience.Hosts._Console.Plugins
-{
-    public interface IConsoleService
-    {
-        Task<string?> ReadLineAsync();
-        void Write(string message);
-        void WriteLine(string message);
-    }
-}
--- Hosts/Service/AppConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Agience.Hosts.Service
-{
-    public class AppConfig
-    {
-        public string? AuthorityUri { get; set; }
-        public string? ClientId { get; set; }
-        public string? ClientSecret { get; set; }
-    }
-}
