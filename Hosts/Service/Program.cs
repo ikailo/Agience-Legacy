b05fa94699fd78e2e102652720a26385681a0acf
Pablo Alessio <pablo@vimvest.com>
2024-05-28 13:14:49 +0200

    Initial Error Handling

--- Hosts/Service/Program.cs
+++ Hosts/Service/Program.cs
@@ -1,27 +1,38 @@
-namespace Agience.Hosts.Service
+namespace Agience.Hosts.Service;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionProcessor;
+
+        var builder = Host.CreateApplicationBuilder(args);
+
+        if (builder.Environment.EnvironmentName == "Development")
         {
-            var builder = Host.CreateApplicationBuilder(args);
-
-            if (builder.Environment.EnvironmentName == "Development")
-            {
-                builder.Configuration.AddUserSecrets<Program>();
-            }
-
-            var intermediateServiceProvider = builder.Services.BuildServiceProvider();
-            var configuration = intermediateServiceProvider.GetRequiredService<IConfiguration>();
-
-            var appConfig = new AppConfig();
-            configuration.Bind(appConfig);
-            builder.Services.AddSingleton(appConfig);
-
-            builder.Services.AddHostedService<Worker>();
-
-            var host = builder.Build();
-            host.Run();
+            builder.Configuration.AddUserSecrets<Program>();
         }
+
+        var intermediateServiceProvider = builder.Services.BuildServiceProvider();
+        var configuration = intermediateServiceProvider.GetRequiredService<IConfiguration>();
+
+        var appConfig = new AppConfig();
+        configuration.Bind(appConfig);
+        builder.Services.AddSingleton(appConfig);
+
+        builder.Services.AddHostedService<Worker>();
+
+        var host = builder.Build();
+        host.Run();
+    }
+
+    static void UnhandledExceptionProcessor(object sender, UnhandledExceptionEventArgs e)
+    {
+        //Any action here...
+        //Implement Logging here...
+
+        //Temp
+        Console.WriteLine("\n\n Unhandled Exception: " + e.ExceptionObject.ToString());
     }
 }
+
