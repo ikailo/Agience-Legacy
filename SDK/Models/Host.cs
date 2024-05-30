b431a23712bdc68ba00c77d67a2be087c63271ec
john-s4d <john@ikailo.com>
2024-05-30 15:58:14 -0400

    Small fix for Models

--- SDK/Models/Host.cs
+++ SDK/Models/Host.cs
@@ -5,5 +5,15 @@ namespace Agience.SDK.Models
     public class Host : AgienceObject
     {
 
+        [JsonPropertyName("plugin")]
+        public Plugin? Plugin { get; set; }
+
+        [JsonIgnore]
+        public string? PluginId
+        {
+            get { return Plugin?.Id; }
+            set { Plugin = SetId(Plugin, value); }
+        }
+
     }
 }
