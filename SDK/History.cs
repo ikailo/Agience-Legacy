e9d963017397fe15bc09c1ed59a3cf9f2e1fef1b
john-s4d <john@ikailo.com>
2024-08-31 09:21:03 -0400

    clean-up

--- SDK/History.cs
+++ SDK/History.cs
@@ -8,10 +8,9 @@ namespace Agience.SDK
         public string? Id { get; internal set; }
         public Data? Input { get; internal set; }
         public DateTime? InputTimestamp { get; internal set; }
-        public Data? Output { get; internal set; } // Type?
+        public Data? Output { get; internal set; }
         public DateTime? OutputTimestamp { get; internal set; }
-        public Data? Transformation { get; internal set; } // KernelFunction?
-        //public string? TemplateId { get; internal set; } // TODO: This should be implict to the Transformation.
+        public string? FunctionId { get; internal set; }
     }
 
     public class InformationEdge : Edge<InformationVertex>
@@ -25,8 +24,6 @@ namespace Agience.SDK
         public string? OwnerId { get; internal set; }
 
         private readonly ConcurrentDictionary<string, object> _vertexLocks = new ConcurrentDictionary<string, object>();
-        //private readonly ConcurrentDictionary<string, InformationVertex> _verticesById = new ConcurrentDictionary<string, InformationVertex>();
-
         private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
 
         public History(string? id = null, string? ownerId = null)
@@ -58,17 +55,17 @@ namespace Agience.SDK
                             InputTimestamp = DateTime.TryParse(information.InputTimestamp!, out var inputTimestamp) ? inputTimestamp : null,
                             Output = information.Output,
                             OutputTimestamp = DateTime.TryParse(information.OutputTimestamp!, out var outputTimestamp) ? outputTimestamp : null,
-                            Transformation = information.Transformation,
+                            FunctionId = information.FunctionId,
                         };
                         AddVertex(currentVertex);
                     }
-                    else // Information builds in stages, but once the value is there it is immutable. Safe to overwrite.
+                    else // Information builds in stages, but once the value is there it should be immutable. Safe to overwrite.
                     {
                         currentVertex.Input = information.Input;
                         currentVertex.InputTimestamp = DateTime.TryParse(information.InputTimestamp!, out var inputTimestamp) ? inputTimestamp : null;
                         currentVertex.Output = information.Output;
                         currentVertex.OutputTimestamp = DateTime.TryParse(information.OutputTimestamp!, out var outputTimestamp) ? outputTimestamp : null;
-                        currentVertex.Transformation = information.Transformation;
+                        currentVertex.FunctionId = information.FunctionId;
                     }
                 }
                 finally
@@ -77,9 +74,9 @@ namespace Agience.SDK
                 }
             }
 
-            if (string.IsNullOrEmpty(information.ParentInformationId)) { return; }
+            if (string.IsNullOrEmpty(information.ParentId)) { return; }
 
-            var parentVertexLock = _vertexLocks.GetOrAdd(information.ParentInformationId, _ => new object());
+            var parentVertexLock = _vertexLocks.GetOrAdd(information.ParentId, _ => new object());
 
             lock (parentVertexLock)
             {
@@ -87,20 +84,19 @@ namespace Agience.SDK
                 try
                 {
                     // TODO: Use indexed dictionary for performance
-                    var parentVertex = Vertices.FirstOrDefault(v => v.Id == information.ParentInformationId);
+                    var parentVertex = Vertices.FirstOrDefault(v => v.Id == information.ParentId);
 
                     if (parentVertex == null)
                     {
                         parentVertex = new InformationVertex()
                         {
-                            Id = information.ParentInformationId
+                            Id = information.ParentId
                         };
                         AddVertex(parentVertex);
                     }
 
                     AddEdge(new InformationEdge(parentVertex, currentVertex));
 
-                    //var foo = this;
                 }
                 finally
                 {
