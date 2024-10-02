46c55151e8aeb2069dc6641f5c0ad498c05feb30
john-s4d <john@ikailo.com>
2024-10-02 18:36:57 -0400

    pre-publish

--- Hosts/Console/Program.cs
+++ Hosts/Console/Program.cs
@@ -2,77 +2,76 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel.Plugins.Core;
 using Microsoft.Extensions.Logging;
 using Agience.Plugins.Primary.OpenAI;
-using Microsoft.SemanticKernel.Plugins.Core;
-using Agience.Plugins.Primary.Interaction;
 
 namespace Agience.Hosts._Console
 {
     internal class Program
     {
         private static ILogger<Program>? _logger;
-        private static string _contextAgentId = string.Empty;
 
         internal static async Task Main(string[] args)
         {
-            var app = CreateHostBuilder(args).Build();
+            var appBuilder = Host.CreateApplicationBuilder(args);
 
-            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+            appBuilder.Logging.ClearProviders();
+            appBuilder.Logging.AddConsole();
+            
+            // appBuilder.Logging.AddDebug();
 
-            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
-            {
-                logger.LogError($"Unhandled Exception occurred: {e.ExceptionObject}");
-            };
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionProcessor;            
+
+            appBuilder.Configuration.AddUserSecrets<AppConfig>();
+
+            var config = appBuilder.Configuration.Get<AppConfig>() ?? new AppConfig();
+
+            // TODO: These checks might not be necessary since we'll check in the builder anyway
+            //if (string.IsNullOrWhiteSpace(config.HostName)) { throw new ArgumentNullException("HostName"); }
+            if (string.IsNullOrWhiteSpace(config.AuthorityUri)) { throw new ArgumentNullException("AuthorityUri"); }
+            if (string.IsNullOrWhiteSpace(config.HostId)) { throw new ArgumentNullException("HostId"); }
+            if (string.IsNullOrWhiteSpace(config.HostSecret)) { throw new ArgumentNullException("HostSecret"); }
+            if (string.IsNullOrWhiteSpace(config.OpenAiApiKey)) { throw new ArgumentNullException("OpenAiApiKey"); }
+                        
+            // Add Agience Host
+            appBuilder.Services.AddAgienceHost(config.AuthorityUri, config.HostId, config.HostSecret, config.CustomNtpHost, null, null, config.OpenAiApiKey);
+
+            appBuilder.Services.AddTransient<AgienceConsoleHost>();
+
+            var app = appBuilder.Build();
+
+            _logger = app.Services.GetRequiredService<ILogger<Program>>();
 
             // ** Configure the Agience Host ** //
 
             var agienceHost = app.Services.GetRequiredService<SDK.Host>();
 
-            agienceHost.Services.AddSingleton<IConsoleService, AgienceConsolePluginService>();
-
 #pragma warning disable SKEXP0050
             // TODO: These plugins should be loaded dynamically during runtime.
             agienceHost.AddPluginFromType<TimePlugin>("msTime");
-            agienceHost.AddPluginFromType<ConsolePlugin>("agienceConsole");
             agienceHost.AddPluginFromType<ChatCompletionPlugin>("openAiChatCompletion");
 #pragma warning restore SKEXP0050
 
-            // ** Start the Agience Host and Console ** //
+
+            // TODO: Add plugins from a local assembly directory (startup and runtime)
+            // TODO: Add plugins initiated from Authority (startup and runtime)
+            // TODO: Register local services and plugins.;
 
             try
             {
                 await agienceHost.StartAsync();
-                await app.Services.GetRequiredService<AgienceConsoleService>().RunAsync();
+                await app.Services.GetRequiredService<AgienceConsoleHost>().Run();
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, "An error occurred while running the application.");
+                _logger.LogError(ex, "An error occurred while running the application.");
             }
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    config.AddUserSecrets<Program>();
-                })
-                .ConfigureServices((context, services) =>
-                {
-                    var config = context.Configuration.Get<AppConfig>() ?? new AppConfig();
-
-                    if (string.IsNullOrWhiteSpace(config.AuthorityUri)) { throw new ArgumentNullException(nameof(config.AuthorityUri)); }
-                    if (string.IsNullOrWhiteSpace(config.HostId)) { throw new ArgumentNullException(nameof(config.HostId)); }
-                    if (string.IsNullOrWhiteSpace(config.HostSecret)) { throw new ArgumentNullException(nameof(config.HostSecret)); }
-
-                    services.AddAgienceHost(config.AuthorityUri, config.HostId, config.HostSecret, config.CustomNtpHost, null, null, config.HostOpenAiApiKey);
-                    services.AddSingleton<AgienceConsoleService>();
-                })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                });
+        static void UnhandledExceptionProcessor(object sender, UnhandledExceptionEventArgs e)
+        {
+            _logger?.LogError($"\n\n Unhandled Exception occurred: {e.ExceptionObject}");
+        }
     }
 }
